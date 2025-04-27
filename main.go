95dcf8439c9988f74c33edbdf4ce4a5eef311710
Matheus <matheuscruz500@gmail.com>
2025-04-27 18:29:05 -0300

    DOESNT fail lint check

--- main.go
+++ main.go
@@ -17,10 +17,7 @@ import (
 	_ "github.com/tursodatabase/libsql-client-go/libsql"
 )
 
-func unused() {
-	// this function does nothing
-	// and is called nowhere
-}
+
 
 type apiConfig struct {
 	DB *database.Queries
